c43e098d00e9a154399f52441b84dd770e8e6edd
Akib Uz Zaman <akib.zaman.04@gmail.com>
2021-09-16 13:15:35 +0600

    fix local storage

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -6,7 +6,7 @@ function ProductCard() {
     const [products, setProducts] = useState([])
     const { cartItems, addToCart } = useContext(CartContext)
 
-    if(localStorage.getItem('products') === null){
+    if(JASON.parse(localStorage.getItem('products')) === null){
         GetProducts()
     }
     

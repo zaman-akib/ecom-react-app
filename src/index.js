b274ca3a56c69158f70fdff5c78d8b3263f0ccb4
Akib Uz Zaman <akib.zaman.04@gmail.com>
2021-09-16 12:55:17 +0600

    Revert "fix router issue"
    This reverts commit ccf915208ea9008e174b8ecddcfa870d7562ed73.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import EcomApp from './components/Main/EcomApp';
 import CartState from './context/CartContext/CartState'
-import RouteHandler from './Router/RouteHandler';
+import RouteHandler from './router/RouteHandler';
 
 ReactDOM.render(
   <React.StrictMode>
